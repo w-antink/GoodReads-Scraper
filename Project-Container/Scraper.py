f76fab0187e3e121e18d99d50f574dffa0199746
WillA656 <wtantink@gmail.com>
2024-12-27 17:49:49 -0800

    .

---
+++ Project-Container/Scraper.py
@@ -0,0 +1,14 @@
+#IMPORTS==================================
+import selenium # type: ignore
+import re #Necessary for the conv_names function
+import time
+import json
+from selenium import webdriver  
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+#=========================================
+
+#SCRAPER==================================
+
+#=========================================
