b5e25bf98c604779e02dcc26bf495d411e24702b
WillA656 <wtantink@gmail.com>
2024-12-28 23:12:51 -0800

    Update Scraper.py

--- Project-Container/Scraper.py
+++ Project-Container/Scraper.py
@@ -15,6 +15,41 @@ options = Options()
 options.headless = False  # don't trust the user to not mess with the slides
 driver = webdriver.Firefox()
 
+
+def scrape_book(driver, type): #Grabs all of the data from a given book's page.
+
+    title = driver.find_element(By.CLASS_NAME, 'Text__title1').text
+    print(title)
+
+    #Grabs just the author name. Very dirty and not built for edge cases yet.
+    authors = []
+    for auth_element in driver.find_elements(By.CLASS_NAME, 'ContributorLink__name'):
+        authors.append(auth_element.text)
+        
+    author = authors[0]
+    print(author)
+
+    addtl_authors = authors[1:-1]
+    print(addtl_authors)
+
+    shelf_location = type
+    print(shelf_location)
+
+    #genres = '' #Not sure how to implement this.
+
+    publish_date = (driver.find_element(By.CLASS_NAME, 'FeaturedDetails').text).split("\n")[-1].replace("First published ", "")
+    print(publish_date)
+
+    try:
+        series = (driver.find_element(By.CLASS_NAME, 'Text__title3').text).split("in the")[-1].strip().rsplit("series", 1)[0].strip()
+        number_in_series = int((driver.find_element(By.CLASS_NAME, 'Text__title3').text).split()[1])
+        print(series, number_in_series)
+    except:
+        pass
+    reading_start = ''
+    reading_end = ''
+    print('\n//////\n')
+
 def run_scraper(driver):
     driver.get('https://www.goodreads.com/ap/signin?language=en_US&openid.assoc_handle=amzn_goodreads_web_na&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.goodreads.com%2Fap-handler%2Fsign-in&siteState=eyJyZXR1cm5fdXJsIjoiaHR0cHM6Ly93d3cuZ29vZHJlYWRzLmNvbS8ifQ%3D%3D')
     
@@ -32,39 +67,6 @@ def run_scraper(driver):
         driver.get(f'https://www.goodreads.com/review/list/102007809-jeannette-antink?shelf={type}')
         print(f'Scraping for {type}')
 
-        #Below is grabbing all of the data on each book.
-        title = driver.find_element(By.CLASS_NAME, 'Text__title1').text
-        print(title)
-
-        #Grabs just the author name. Very dirty and not built for edge cases yet.
-        authors = []
-        for auth_element in driver.find_elements(By.CLASS_NAME, 'ContributorLink__name'):
-            authors.append(auth_element.text)
-        
-        author = authors[0]
-        print(author)
-
-        addtl_authors = authors[1:-1]
-        print(addtl_authors)
-
-        shelf_location = type
-        print(shelf_location)
-
-        #genres = '' #Not sure how to implement this.
-
-        publish_date = (driver.find_element(By.CLASS_NAME, 'FeaturedDetails').text).split("\n")[-1].replace("First published ", "")
-        print(publish_date)
-
-        try:
-            series = (driver.find_element(By.CLASS_NAME, 'Text__title3').text).split("in the")[-1].strip().rsplit("series", 1)[0].strip()
-            number_in_series = int((driver.find_element(By.CLASS_NAME, 'Text__title3').text).split()[1])
-            print(series, number_in_series)
-        except:
-            pass
-        reading_start = ''
-        reading_end = ''
-        print('\n//////\n')
-    
     
 
 #=========================================
